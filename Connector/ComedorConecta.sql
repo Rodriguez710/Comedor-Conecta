96261cf8fb97f9407cfb9b03d2c37c7d782d62a0
dgp04 <164475141+dgp04@users.noreply.github.com>
2024-05-14 18:22:26 +0200

    Update ComedorConecta.sql
    Añadimos los datos a la tabla Cursos y Clase para el correcto funcionamiento de las claves ajenas ya que son valores constantes.

--- Connector/ComedorConecta.sql
+++ Connector/ComedorConecta.sql
@@ -12,24 +12,36 @@ telefono 	varchar(15)		null
 );
 
 create table cursos(
-curso		int				primary key not null,
-centro		varchar(200) 	not null
+curso		int				primary key not null
 );
+insert into cursos values (1);
+insert into cursos values (2);
+insert into cursos values (3);
+insert into cursos values (4);
+insert into cursos values (5);
+insert into cursos values (6);
 
 create table clase(
-letra	char	primary key,
+letra	char	not null primary key,
 curso 	int		not null,
 constraint fk_curso_clase foreign key (curso) references cursos (curso)
 	on delete cascade on update cascade
 );
+insert into clase (letra, curso) values ('A', 1);
+insert into clase (letra, curso) values ('B', 2);
+insert into clase (letra, curso) values ('C', 3);
+insert into clase (letra, curso) values ('D', 4);
+insert into clase (letra, curso) values ('E', 5);
+insert into clase (letra, curso) values ('F', 6);
+
 
 create table alumno(
-nre			int				primary key,
+nre			int				primary key unique,
 nombre		varchar(200)	not null,
-letra_clase	char			not null,
 curso		int				not null,
-madre		varchar(200)		not null,
-constraint fk_clase foreign key (letra_clase) references clase(letra)
+clase		char			not null,
+madre		varchar(200)	not null,
+constraint fk_clase foreign key (clase) references clase(letra)
 	on delete cascade on update cascade
 );
 
@@ -40,3 +52,5 @@ nre_hijo 	int				not null,
 constraint fk_nre_hijo foreign key (nre_hijo) references alumno (nre)
 	on delete cascade on update cascade
 );
+
+select * from alumno;
