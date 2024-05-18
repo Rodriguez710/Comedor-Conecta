c9ff6f71ea70c5c0b9c74fdb64eb8d3e63b15b20
dgp04 <164475141+dgp04@users.noreply.github.com>
2024-05-18 18:07:59 +0200

    Update AlumnoConnector.py
    Añadimos el método para borrar varios alumnos a la vez

--- Connector/AlumnoConnector.py
+++ Connector/AlumnoConnector.py
@@ -1,4 +1,5 @@
 import pymysql
+from PySide6.QtCore import Qt
 
 class AlumnoConnector():
     def __init__(self):
@@ -38,11 +39,26 @@ class AlumnoConnector():
         sql = "update alumno set nombre = %s, curso = %s, clase = %s, madre = %s where nre = %s".format(nombre, curso, clase, madre, nre)
         self.cursor.execute(sql, (nombre, curso, clase, madre, nre))
         self.con.commit()
-    
-    def borrarAlumno(self, nre):
-        sql = "delete from alumno where nre = %s".format(nre)
-        self.cursor.execute(sql, (nre))
-        self.con.commit()
+
+    def eliminar_alumnos_seleccionados(self, nres):
+        if not nres:
+            print("No se han seleccionado alumnos para eliminar.")
+            return
+
+        try:
+            placeholders = ', '.join(['%s'] * len(nres))
+            sql = f"DELETE FROM alumno WHERE nre IN ({placeholders})"
+            
+            connection = pymysql.connect(host='localhost', user='root', password='root', db='ComedorConecta')
+            cursor = connection.cursor()
+            cursor.execute(sql, nres)
+            connection.commit()
+            print(f"Se han eliminado {cursor.rowcount} alumnos.")
+        except Exception as e:
+            print(f"Error al eliminar alumnos: {str(e)}")
+        finally:
+            cursor.close()
+            connection.close()
     
     def devuelvePorCurso(self, curso):
         try:
