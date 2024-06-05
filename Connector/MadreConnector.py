e079d21d4e6c481a1811d97f5ac4debe6a2676f0
dgp04 <164475141+dgp04@users.noreply.github.com>
2024-06-05 14:40:24 +0200

    Update MadreConnector.py

--- Connector/MadreConnector.py
+++ Connector/MadreConnector.py
@@ -10,14 +10,14 @@ class MadreConnector():
         )
         self.cursor = self.con.cursor()
         
-    def insertar(self, nombre, nre_hijo, email, direccion):
-        sql = "insert into madre (nombre, nre_hijo, email, direccion) values (%s, %s, %s, %s)".format(nombre, nre_hijo, email, direccion)
-        self.con.cursor().execute(sql, (nombre, nre_hijo, email, direccion))
+    def insertar(self, nombre, num_hijo, email, direccion):
+        sql = "insert into madre (nombre, num_hijo, email, direccion) values (%s, %s, %s, %s)".format(nombre, num_hijo, email, direccion)
+        self.con.cursor().execute(sql, (nombre, num_hijo, email, direccion))
         self.con.commit()
        
     def devuelveTodos(self):
         try:
-            sql = "SELECT id, nombre, nre_hijo, email, direccion FROM madre"
+            sql = "SELECT id, nombre, num_hijo, email, direccion FROM madre"
             self.cursor.execute(sql)
             madres = self.cursor.fetchall()
             return madres
@@ -27,16 +27,16 @@ class MadreConnector():
     
     def devuelvePorID(self, id):
         try:
-            sql = "SELECT id, nombre, nre_hijo, email, direccion FROM madre where id = %s".format(id)
+            sql = "SELECT id, nombre, num_hijo, email, direccion FROM madre where id = %s".format(id)
             self.cursor.execute(sql, (id))
             madre = self.cursor.fetchone()
             return madre
         except Exception as e:
             print(f"Error al obtener la madre: {str(e)}")
     
-    def actualizarMadre(self, nombre, nre_hijo, email, direccion, id):
-        sql = "update madre set nombre = %s, nre_hijo = %s, email = %s, direccion = %s where id = %s".format(nombre, nre_hijo, email, direccion, id)
-        self.cursor.execute(sql, (nombre, nre_hijo, email, direccion, id))
+    def actualizarMadre(self, nombre, num_hijo, email, direccion, id):
+        sql = "update madre set nombre = %s, num_hijo = %s, email = %s, direccion = %s where id = %s".format(nombre, num_hijo, email, direccion, id)
+        self.cursor.execute(sql, (nombre, num_hijo, email, direccion, id))
         self.con.commit()
     
     def borrarMadre(self, id):
@@ -46,7 +46,7 @@ class MadreConnector():
         
     def devuelvePorID(self, id):
         try:
-            sql = "SELECT id, nombre, nre_hijo, email, direccion FROM madre WHERE id = %s"
+            sql = "SELECT id, nombre, num_hijo, email, direccion FROM madre WHERE id = %s"
             self.cursor.execute(sql, (id,))
             madre = self.cursor.fetchone()
             return madre
@@ -54,9 +54,9 @@ class MadreConnector():
             print(f"Error al obtener las madres del curso: {str(e)}")
             return None
     
-    def existeNRE(self, nre_hijo):
-        sql = "SELECT COUNT(*) FROM madre WHERE nre_hijo = %s"
-        self.cursor.execute(sql, (nre_hijo,))
+    def existeNRE(self, num_hijo):
+        sql = "SELECT COUNT(*) FROM madre WHERE num_hijo = %s"
+        self.cursor.execute(sql, (num_hijo,))
         count = self.cursor.fetchone()[0]
         return count > 0
         
@@ -77,10 +77,10 @@ class MadreConnector():
             self.cursor.close()
             self.con.close()
             
-    def devuelvePorNREHijo(self, nre):
+    def devuelvePorNREHijo(self, numero):
         try:
-            sql = "SELECT id, nombre, nre_hijo, email, direccion FROM madre WHERE nre_hijo = %s".format(nre)
-            self.cursor.execute(sql, (nre))
+            sql = "SELECT id, nombre, num_hijo, email, direccion FROM madre WHERE num_hijo = %s".format(numero)
+            self.cursor.execute(sql, (numero))
             madre = self.cursor.fetchone()
             return madre
         except Exception as e:
