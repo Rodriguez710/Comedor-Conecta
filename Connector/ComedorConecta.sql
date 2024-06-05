434ad999d97abec9889ee2491af2460cc52dccc9
dgp04 <164475141+dgp04@users.noreply.github.com>
2024-06-05 14:40:48 +0200

    Update ComedorConecta.sql

--- Connector/ComedorConecta.sql
+++ Connector/ComedorConecta.sql
@@ -14,44 +14,30 @@ telefono 	varchar(15)		null
 create table cursos(
 curso		varchar(5)				primary key not null
 );
-insert into cursos values ('1º');
-insert into cursos values ('2º');
-insert into cursos values ('3º');
-insert into cursos values ('4º');
-insert into cursos values ('5º');
-insert into cursos values ('6º');
-
-create table clase(
-letra	char	not null primary key,
-curso 	varchar(5)		not null,
-constraint fk_curso_clase foreign key (curso) references cursos (curso)
-	on delete cascade on update cascade
-);
-insert into clase (letra, curso) values ('A', '1º');
-insert into clase (letra, curso) values ('B', '2º');
-insert into clase (letra, curso) values ('C', '3º');
-insert into clase (letra, curso) values ('D', '4º');
-insert into clase (letra, curso) values ('E', '5º');
-insert into clase (letra, curso) values ('F', '6º');
+insert into cursos values ('1');
+insert into cursos values ('2');
+insert into cursos values ('3');
+insert into cursos values ('4');
+insert into cursos values ('5');
+insert into cursos values ('6');
 
 
 create table alumno(
-nre			int				primary key unique,
-nombre		varchar(200)	not null collate utf8_bin,
-curso		varchar(5)		not null,
-clase		char			not null,
-madre		varchar(200)	not null collate utf8_bin,
-constraint fk_clase foreign key (clase) references clase(letra)
-	on delete cascade on update cascade
+numero				int				primary key unique,
+primerApellido		varchar(200)	not null collate utf8_bin,
+segundoApellido		varchar(200)	not null collate utf8_bin,
+curso				char			not null,
+ensenanza			varchar(200) 	not null collate utf8_bin,
+grupo				varchar(50)		not null,
+nombre				varchar(200)	not null collate utf8_bin
 );
 
 create table madre(
 id			int 			primary key auto_increment,
 nombre		varchar(200)	not null collate utf8_bin,
-nre_hijo 	int				not null,
+num_hijo 	int				not null,
 email		varchar(100) 	not null collate utf8_bin,
 direccion	varchar(200)	not null collate utf8_bin,	
-constraint fk_nre_hijo foreign key (nre_hijo) references alumno (nre)
+constraint fk_num_hijo foreign key (num_hijo) references alumno (numero)
 	on delete cascade on update cascade
 );
-select * from madre
