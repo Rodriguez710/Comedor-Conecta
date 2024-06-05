3a52ceab1e325ef4db47fd0c16333f0e321ab711
dgp04 <164475141+dgp04@users.noreply.github.com>
2024-06-05 14:38:51 +0200

    Update listadoPadres.py

--- listadoPadres.py
+++ listadoPadres.py
@@ -230,7 +230,7 @@ QHeaderView::section {
         ___qtablewidgetitem1 = self.tableWidget.horizontalHeaderItem(1)
         ___qtablewidgetitem1.setText(QCoreApplication.translate("Dialog_listadp_padres", u"Nombre", None));
         ___qtablewidgetitem2 = self.tableWidget.horizontalHeaderItem(2)
-        ___qtablewidgetitem2.setText(QCoreApplication.translate("Dialog_listadp_padres", u"NRE-Hijo", None));
+        ___qtablewidgetitem2.setText(QCoreApplication.translate("Dialog_listadp_padres", u"Nº Hijo", None));
         ___qtablewidgetitem3 = self.tableWidget.horizontalHeaderItem(3)
         ___qtablewidgetitem3.setText(QCoreApplication.translate("Dialog_listadp_padres", u"Email", None));
         ___qtablewidgetitem4 = self.tableWidget.horizontalHeaderItem(4)
