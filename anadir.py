5618ad1b7a65335de16199e212b22a5df730d45d
dgp04 <164475141+dgp04@users.noreply.github.com>
2024-06-06 12:58:47 +0200

    Add files via upload

--- anadir.py
+++ anadir.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'anadir.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.6.0
+## Created by: Qt User Interface Compiler version 6.6.2
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -26,7 +26,7 @@ class Ui_Dialog_anadir_alumno(QDialog, object):
     def setupUi(self, Dialog, origen):
         if not Dialog.objectName():
             Dialog.setObjectName(u"Dialog")
-        Dialog.resize(583, 468)
+        Dialog.resize(572, 525)
         icon = QIcon()
         icon.addFile(u":/logo/iconoProyecto.png", QSize(), QIcon.Normal, QIcon.Off)
         Dialog.setWindowIcon(icon)
@@ -36,18 +36,16 @@ class Ui_Dialog_anadir_alumno(QDialog, object):
         
         self.origen = origen
         
-        self.layoutWidget = QWidget(Dialog)
-        self.layoutWidget.setObjectName(u"layoutWidget")
-        self.layoutWidget.setGeometry(QRect(10, 10, 561, 451))
-        self.verticalLayout = QVBoxLayout(self.layoutWidget)
+        self.verticalLayout_3 = QVBoxLayout(Dialog)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.verticalLayout = QVBoxLayout()
         self.verticalLayout.setObjectName(u"verticalLayout")
-        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
         self.verticalLayout_2 = QVBoxLayout()
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.verticalLayout_2.setSizeConstraint(QLayout.SetDefaultConstraint)
-        self.logo = QLabel(self.layoutWidget)
+        self.logo = QLabel(Dialog)
         self.logo.setObjectName(u"logo")
-        sizePolicy = QSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Maximum, QSizePolicy.Policy.Maximum)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.logo.sizePolicy().hasHeightForWidth())
@@ -67,7 +65,7 @@ class Ui_Dialog_anadir_alumno(QDialog, object):
         self.formLayout_3.setObjectName(u"formLayout_3")
         self.formLayout_3.setVerticalSpacing(0)
         self.formLayout_3.setContentsMargins(160, -1, -1, -1)
-        self.label_nre = QLabel(self.layoutWidget)
+        self.label_nre = QLabel(Dialog)
         self.label_nre.setObjectName(u"label_nre")
         self.label_nre.setMaximumSize(QSize(102, 16777215))
         font = QFont()
@@ -84,11 +82,10 @@ class Ui_Dialog_anadir_alumno(QDialog, object):
 
         self.formLayout_3.setWidget(0, QFormLayout.LabelRole, self.label_nre)
 
-        self.lineEdit_nre = QLineEdit(self.layoutWidget)
-        self.lineEdit_nre.setObjectName(u"lineEdit_nre")
-        self.lineEdit_nre.setMaxLength(7)
+        self.lineEdit_nre = QLineEdit(Dialog)
         self.lineEdit_nre.setValidator(double_validator)
-        sizePolicy1 = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.lineEdit_nre.setObjectName(u"lineEdit_nre")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
         sizePolicy1.setHorizontalStretch(0)
         sizePolicy1.setVerticalStretch(0)
         sizePolicy1.setHeightForWidth(self.lineEdit_nre.sizePolicy().hasHeightForWidth())
@@ -103,10 +100,13 @@ class Ui_Dialog_anadir_alumno(QDialog, object):
 
         self.formLayout_3.setWidget(0, QFormLayout.FieldRole, self.lineEdit_nre)
 
-        self.label_nombre = QLabel(self.layoutWidget)
+        self.label_nombre = QLabel(Dialog)
         self.label_nombre.setObjectName(u"label_nombre")
         self.label_nombre.setMaximumSize(QSize(102, 16777215))
-        self.label_nombre.setFont(font)
+        font1 = QFont()
+        font1.setPointSize(11)
+        font1.setBold(True)
+        self.label_nombre.setFont(font1)
         self.label_nombre.setStyleSheet(u"QLabel{\n"
 "color: white;\n"
 "font-weight: bold;\n"
@@ -119,7 +119,7 @@ class Ui_Dialog_anadir_alumno(QDialog, object):
 
         self.formLayout_3.setWidget(1, QFormLayout.LabelRole, self.label_nombre)
 
-        self.lineEdit_nombre = QLineEdit(self.layoutWidget)
+        self.lineEdit_nombre = QLineEdit(Dialog)
         self.lineEdit_nombre.setObjectName(u"lineEdit_nombre")
         sizePolicy1.setHeightForWidth(self.lineEdit_nombre.sizePolicy().hasHeightForWidth())
         self.lineEdit_nombre.setSizePolicy(sizePolicy1)
@@ -133,10 +133,10 @@ class Ui_Dialog_anadir_alumno(QDialog, object):
 
         self.formLayout_3.setWidget(1, QFormLayout.FieldRole, self.lineEdit_nombre)
 
-        self.label_curso = QLabel(self.layoutWidget)
+        self.label_curso = QLabel(Dialog)
         self.label_curso.setObjectName(u"label_curso")
         self.label_curso.setMaximumSize(QSize(102, 16777215))
-        self.label_curso.setFont(font)
+        self.label_curso.setFont(font1)
         self.label_curso.setStyleSheet(u"QLabel{\n"
 "color: white;\n"
 "font-weight: bold;\n"
@@ -149,7 +149,7 @@ class Ui_Dialog_anadir_alumno(QDialog, object):
 
         self.formLayout_3.setWidget(2, QFormLayout.LabelRole, self.label_curso)
 
-        self.lineEdit_curso = QLineEdit(self.layoutWidget)
+        self.lineEdit_curso = QLineEdit(Dialog)
         self.lineEdit_curso.setObjectName(u"lineEdit_curso")
         sizePolicy1.setHeightForWidth(self.lineEdit_curso.sizePolicy().hasHeightForWidth())
         self.lineEdit_curso.setSizePolicy(sizePolicy1)
@@ -163,7 +163,7 @@ class Ui_Dialog_anadir_alumno(QDialog, object):
 
         self.formLayout_3.setWidget(2, QFormLayout.FieldRole, self.lineEdit_curso)
 
-        self.label_clase = QLabel(self.layoutWidget)
+        self.label_clase = QLabel(Dialog)
         self.label_clase.setObjectName(u"label_clase")
         self.label_clase.setMaximumSize(QSize(102, 16777215))
         self.label_clase.setFont(font)
@@ -179,7 +179,8 @@ class Ui_Dialog_anadir_alumno(QDialog, object):
 
         self.formLayout_3.setWidget(3, QFormLayout.LabelRole, self.label_clase)
 
-        self.lineEdit_clase = QLineEdit(self.layoutWidget)
+        self.lineEdit_clase = QLineEdit(Dialog)
+        self.lineEdit_clase.setValidator(double_validator)
         self.lineEdit_clase.setObjectName(u"lineEdit_clase")
         self.lineEdit_clase.setMaximumSize(QSize(200, 16777215))
         self.lineEdit_clase.setStyleSheet(u"QLineEdit{\n"
@@ -191,12 +192,9 @@ class Ui_Dialog_anadir_alumno(QDialog, object):
 
         self.formLayout_3.setWidget(3, QFormLayout.FieldRole, self.lineEdit_clase)
 
-        self.label_padres = QLabel(self.layoutWidget)
+        self.label_padres = QLabel(Dialog)
         self.label_padres.setObjectName(u"label_padres")
         self.label_padres.setMaximumSize(QSize(102, 16777215))
-        font1 = QFont()
-        font1.setPointSize(14)
-        font1.setBold(True)
         self.label_padres.setFont(font1)
         self.label_padres.setStyleSheet(u"QLabel{\n"
 "color: white;\n"
@@ -210,7 +208,7 @@ class Ui_Dialog_anadir_alumno(QDialog, object):
 
         self.formLayout_3.setWidget(4, QFormLayout.LabelRole, self.label_padres)
 
-        self.lineEdit_padres = QLineEdit(self.layoutWidget)
+        self.lineEdit_padres = QLineEdit(Dialog)
         self.lineEdit_padres.setObjectName(u"lineEdit_padres")
         self.lineEdit_padres.setMaximumSize(QSize(200, 16777215))
         self.lineEdit_padres.setStyleSheet(u"QLineEdit{\n"
@@ -222,13 +220,70 @@ class Ui_Dialog_anadir_alumno(QDialog, object):
 
         self.formLayout_3.setWidget(4, QFormLayout.FieldRole, self.lineEdit_padres)
 
+        self.lineEdit = QLineEdit(Dialog)
+        self.lineEdit.setObjectName(u"lineEdit")
+        self.lineEdit.setMaximumSize(QSize(200, 16777215))
+        self.lineEdit.setStyleSheet(u"QLineEdit{\n"
+"border: 0px;\n"
+"border-bottom: 1px solid black;\n"
+"background-color: transparent;\n"
+"margin-top: 10px;\n"
+"}")
+
+        self.formLayout_3.setWidget(5, QFormLayout.FieldRole, self.lineEdit)
+
+        self.lineEdit_2 = QLineEdit(Dialog)
+        self.lineEdit_2.setObjectName(u"lineEdit_2")
+        self.lineEdit_2.setMaximumSize(QSize(200, 16777215))
+        self.lineEdit_2.setStyleSheet(u"QLineEdit{\n"
+"border: 0px;\n"
+"border-bottom: 1px solid black;\n"
+"background-color: transparent;\n"
+"margin-top: 10px;\n"
+"}")
+
+        self.formLayout_3.setWidget(6, QFormLayout.FieldRole, self.lineEdit_2)
+
+        self.label = QLabel(Dialog)
+        self.label.setObjectName(u"label")
+        self.label.setMaximumSize(QSize(102, 16777215))
+        self.label.setFont(font)
+        self.label.setStyleSheet(u"QLabel{\n"
+"color: white;\n"
+"font-weight: bold;\n"
+"background-color: #2a5c94;\n"
+"border: 1px solid black;\n"
+"border-top: none;\n"
+"padding: 5px 10px;\n"
+"margin-top: 0px;\n"
+"}")
+
+        self.formLayout_3.setWidget(5, QFormLayout.LabelRole, self.label)
+
+        self.label_2 = QLabel(Dialog)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setMaximumSize(QSize(102, 16777215))
+        self.label_2.setFont(font)
+        self.label_2.setStyleSheet(u"QLabel{\n"
+"color: white;\n"
+"font-weight: bold;\n"
+"background-color: #2a5c94;\n"
+"border: 1px solid black;\n"
+"border-top: none;\n"
+"padding: 5px 10px;\n"
+"margin-top: 0px;\n"
+"}")
+
+        self.formLayout_3.setWidget(6, QFormLayout.LabelRole, self.label_2)
+
 
         self.verticalLayout.addLayout(self.formLayout_3)
 
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.horizontalLayout.setContentsMargins(-1, 6, -1, -1)
-        self.btn_anadir = QPushButton(self.layoutWidget)
+        self.btn_anadir = QPushButton(Dialog)
+        self.btn_anadir.clicked.connect(self.anadir_alumno)
         self.btn_anadir.setObjectName(u"btn_anadir")
         sizePolicy1.setHeightForWidth(self.btn_anadir.sizePolicy().hasHeightForWidth())
         self.btn_anadir.setSizePolicy(sizePolicy1)
@@ -236,7 +291,6 @@ class Ui_Dialog_anadir_alumno(QDialog, object):
         font2.setPointSize(15)
         font2.setBold(True)
         self.btn_anadir.setFont(font2)
-        self.btn_anadir.clicked.connect(self.anadir_alumno)
         self.btn_anadir.setCursor(QCursor(Qt.PointingHandCursor))
         self.btn_anadir.setStyleSheet(u"QPushButton{\n"
 "background-color: #2a5c94;\n"
@@ -255,6 +309,9 @@ class Ui_Dialog_anadir_alumno(QDialog, object):
 
         self.verticalLayout.addLayout(self.horizontalLayout)
 
+
+        self.verticalLayout_3.addLayout(self.verticalLayout)
+
         with open('config.json', 'r')as json_file:
             data = json.load(json_file)
         
@@ -268,56 +325,62 @@ class Ui_Dialog_anadir_alumno(QDialog, object):
     def retranslateUi(self, Dialog):
         Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"A\u00f1adir alumno", None))
         self.logo.setText(QCoreApplication.translate("Dialog", u"<html><head/><body><p><img src=\":/logo/logoProyecto.png\"/></p></body></html>", None))
-        self.label_nre.setText(QCoreApplication.translate("Dialog", u"NRE:       ", None))
-        self.lineEdit_nre.setPlaceholderText(QCoreApplication.translate("Dialog", u"Introduce el NRE del alumno...", None))
-        self.label_nombre.setText(QCoreApplication.translate("Dialog", u"Nombre:", None))
-        self.lineEdit_nombre.setPlaceholderText(QCoreApplication.translate("Dialog", u"Introduce el nombre del alumno...", None))
-        self.label_curso.setText(QCoreApplication.translate("Dialog", u"Curso:     ", None))
-        self.lineEdit_curso.setPlaceholderText(QCoreApplication.translate("Dialog", u"Introduce el curso del alumno...", None))
-        self.label_clase.setText(QCoreApplication.translate("Dialog", u"Clase:      ", None))
-        self.lineEdit_clase.setPlaceholderText(QCoreApplication.translate("Dialog", u"Introduce la clase del alumno...", None))
-        self.label_padres.setText(QCoreApplication.translate("Dialog", u"Padre:           ", None))
-        self.lineEdit_padres.setPlaceholderText(QCoreApplication.translate("Dialog", u"Introduce padre/madre del alumno...", None))
+        self.label_nre.setText(QCoreApplication.translate("Dialog", u"N\u00famero:       ", None))
+        self.lineEdit_nre.setPlaceholderText(QCoreApplication.translate("Dialog", u"Introduce el n\u00famero del alumno...", None))
+        self.label_nombre.setText(QCoreApplication.translate("Dialog", u"Apellido 1:", None))
+        self.lineEdit_nombre.setPlaceholderText(QCoreApplication.translate("Dialog", u"Introduce 1er apellido del alumno...", None))
+        self.label_curso.setText(QCoreApplication.translate("Dialog", u"Apellido 2:", None))
+        self.lineEdit_curso.setPlaceholderText(QCoreApplication.translate("Dialog", u"Introduce 2\u00ba apellido del alumno...", None))
+        self.label_clase.setText(QCoreApplication.translate("Dialog", u"Curso:     ", None))
+        self.lineEdit_clase.setPlaceholderText(QCoreApplication.translate("Dialog", u"Introduce el curso del alumno...", None))
+        self.label_padres.setText(QCoreApplication.translate("Dialog", u"Ense\u00f1anza:", None))
+        self.lineEdit_padres.setPlaceholderText(QCoreApplication.translate("Dialog", u"Introduce ense\u00f1anza del alumno...", None))
+        self.lineEdit.setPlaceholderText(QCoreApplication.translate("Dialog", u"Introduce el grupo del alumno...", None))
+        self.lineEdit_2.setPlaceholderText(QCoreApplication.translate("Dialog", u"Introduce el nombre del alumno...", None))
+        self.label.setText(QCoreApplication.translate("Dialog", u"Grupo:     ", None))
+        self.label_2.setText(QCoreApplication.translate("Dialog", u"Nombre:", None))
         self.btn_anadir.setText(QCoreApplication.translate("Dialog", u"A\u00f1adir alumno", None))
     # retranslateUi
 
     def anadir_alumno(self):
-        nre = str(self.lineEdit_nre.text())
-        nombre = self.lineEdit_nombre.text()
-        curso = self.lineEdit_curso.text()
-        clase = self.lineEdit_clase.text()
-        padre = self.lineEdit_padres.text()
-        conector_alumno = AlumnoConnector()
-        alumno = conector_alumno.devuelvePorNRE(nre)
-        
-        if alumno is not None:
-            QMessageBox.critical(self, 'Error', 'El NRE introducido pertenece a un alumno ya registrado.')
-            self.reiniciar()
-        if curso not in('1º', '2º', '3º', '4º', '5º', '6º'):
-            QMessageBox.warning(self, 'Error', 'El curso debe ser de 1º a 6º.')
-        else:
-            if self.origen == 'curso':
-                if curso != self.curso:
-                    QMessageBox.critical(self, 'Error', f'El alumno no puede ser de un curso diferente a {self.curso}')
-                    self.reiniciar()
-                else: 
+            numero = str(self.lineEdit_nre.text())
+            primerApellido = self.lineEdit_nombre.text()
+            segundoApellido = self.lineEdit_curso.text()
+            curso = self.lineEdit_clase.text()
+            ensenanza = self.lineEdit_padres.text()
+            grupo = self.lineEdit.text()
+            nombre = self.lineEdit_2.text()
+            conector_alumno = AlumnoConnector()
+            alumno = conector_alumno.devuelvePorNumero(numero)
+            
+            if alumno is not None:
+                QMessageBox.critical(self, 'Error', 'El NRE introducido pertenece a un alumno ya registrado.')
+                self.reiniciar()
+            if curso not in('1', '2', '3', '4', '5', '6'):
+                QMessageBox.warning(self, 'Error', 'El curso debe ser de 1º a 6º.')
+            else:
+                if self.origen == 'curso':
+                    if curso != self.curso:
+                        QMessageBox.critical(self, 'Error', f'El alumno no puede ser de un curso diferente a {self.curso}º')
+                        self.reiniciar()
+                    else: 
+                        try:
+                            conector_alumno.insertar(numero, primerApellido, segundoApellido, curso, ensenanza, grupo, nombre)
+                            QMessageBox.information(self.btn_anadir, "Éxito", "Alumno añadido con éxito.")
+                            self.accept()
+
+                        except Exception as e:
+                            QMessageBox.critical(self.btn_anadir, "Error", f"Error al insertar datos en la base de datos: {str(e)}")
+                            self.reiniciar()
+                elif self.origen == 'listado':
                     try:
-                        conector_alumno.insertar(nre, nombre, curso, clase, padre)
+                        conector_alumno.insertar(numero, primerApellido, segundoApellido, curso, ensenanza, grupo, nombre)
                         QMessageBox.information(self.btn_anadir, "Éxito", "Alumno añadido con éxito.")
                         self.accept()
 
                     except Exception as e:
                         QMessageBox.critical(self.btn_anadir, "Error", f"Error al insertar datos en la base de datos: {str(e)}")
                         self.reiniciar()
-            elif self.origen == 'listado':
-                try:
-                    conector_alumno.insertar(nre, nombre, curso, clase, padre)
-                    QMessageBox.information(self.btn_anadir, "Éxito", "Alumno añadido con éxito.")
-                    self.accept()
-
-                except Exception as e:
-                    QMessageBox.critical(self.btn_anadir, "Error", f"Error al insertar datos en la base de datos: {str(e)}")
-                    self.reiniciar()
     
     def reiniciar(self):
         self.lineEdit_nombre.clear()
@@ -325,3 +388,5 @@ class Ui_Dialog_anadir_alumno(QDialog, object):
         self.lineEdit_padres.clear()
         self.lineEdit_clase.clear()
         self.lineEdit_curso.clear()
+        self.lineEdit.clear()
+        self.lineEdit_2.clear()
